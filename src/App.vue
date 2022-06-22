dd1879ef578aea974b86e2792680b9fe2f82b4a6
Andrés Perdomo <alvarado0925@gmail.com>
2022-06-22 17:15:12 -0500

    Crear e importar el componente de TablasPersonas

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Personas</h1>
+  <TablaPersonas/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TablaPersonas from '@/components/TablaPersonas.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TablaPersonas
   }
 }
 </script>
